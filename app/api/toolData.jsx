455ada4512ec22090636bbd0897c62414dfb6014
bill r <jy@ozzis.us>
2017-07-23 17:29:30 -0400

    capitalization schnoogling

--- app/api/toolData.jsx
+++ app/api/toolData.jsx
@@ -8,7 +8,7 @@ var toolData = function(){
 			{ "tool": "Axios", "logo":  "axios.png", "url": "https://github.com/mzabriskie/axios", "source": "npm.js", "sourceUrl": "https://www.npmjs.com/package/axios", "desc" : "Promise based HTTP client for the browser and node.js."},
 			{ "tool": "Foundation", "logo":  "foundation.png", "url": "http://foundation.zurb.com/", "source": "wikipedia.org", "sourceUrl": "https://en.wikipedia.org/wiki/Foundation_(framework)", "desc" : "Foundation is a responsive front-end framework. Foundation provides a responsive grid and HTML and CSS UI components, templates, and code snippets..."},
 			{ "tool": "Webpack", "logo":  "webpack.png", "url": "https://webpack.js.org/", "source": "wikipedia.org", "sourceUrl": "https://en.wikipedia.org/wiki/Webpack", "desc" : "Webpack is an open-source JavaScript module bundler."},
-			{ "tool": "Github", "logo":  "Octocat.png", "url": "https://github.com/", "source": "wikipedia.org", "sourceUrl": "https://en.wikipedia.org/wiki/GitHub", "desc" : "GitHub is a web-based Git or version control repository and Internet hosting service."},
+			{ "tool": "Github", "logo":  "octocat-1.png", "url": "https://github.com/", "source": "wikipedia.org", "sourceUrl": "https://en.wikipedia.org/wiki/GitHub", "desc" : "GitHub is a web-based Git or version control repository and Internet hosting service."},
 			{ "tool": "Heroku", "logo":  "heroku-2.png", "url": "http://heroku.com/", "source": "wikipedia.org", "sourceUrl": "https://en.wikipedia.org/wiki/Heroku", "desc" : "Heroku is a cloud platform as a service (PaaS) supporting several programming languages that is used as a web application deployment model."}
 		]
 	};
